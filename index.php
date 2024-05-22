6fa923c26bdb31af4dbc7812b80c16ce1a5fbcbc
safiylm <121391404+safiylm@users.noreply.github.com>
2024-05-22 16:38:36 +0200

    remove dropdown in nav

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <title> Article & Quizz  </title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles/articlesListe.scss"/>
+    <link rel="stylesheet" href="styles/articlesListe.css"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
@@ -24,16 +24,6 @@
           <a class="nav-link" href="/auth/index.php">Connexion</a>
         </li>
        
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown link
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
       </ul>
     </div>
   </div>
