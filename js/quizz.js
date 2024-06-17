9cce037e81c9618ac3d508b499cc94ed8b1782f0
safiylm <121391404+safiylm@users.noreply.github.com>
2024-06-17 20:18:43 +0200

    get resultat of quizz js

--- js/quizz.js
+++ js/quizz.js
@@ -1,15 +1,16 @@
 let quizz_array = [];
 
 function loadQuizz() {
-
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const xmlhttp = new XMLHttpRequest();
     const id = urlParams.get('id')
 
+
     xmlhttp.onload = function () {
+
         const myObj = JSON.parse(this.responseText);
-        let res = myObj.listeOfQuizz.filter(e => e.articleId == id);
+        let res = myObj.listeOfQuizz.filter(e => e.id == id);
         quizz_array = res;
         let reponse = ""
         let q = ""
@@ -19,8 +20,8 @@ function loadQuizz() {
             q = "<p><strong> " + element.question + "</strong></p>";
 
             element.reponses_possible.forEach((r, j) => {
-                qr = qr + "<div class=\"form-check\">"+
-                "<input class=\"form-check-input\" type=\"radio\" name=\"question" + i + "\"  value='" + r + "' id='reponse" + j + "'>"
+                qr = qr + "<div class=\"form-check\">" +
+                    "<input class=\"form-check-input\" type=\"radio\" name=\"question" + i + "\"  value='" + String(r) + "' id='reponse" + j + "'>"
                     + ' <label class="form-check-label" for="reponse' + j + '" id="labelq' + i + 'r' + j + '">'
                     + r + "</label> </div>"
                 //On recupere toutes les reponses possibles on les concat
@@ -31,30 +32,33 @@ function loadQuizz() {
         })
 
         document.getElementById("display-quizz").innerHTML =
-            reponse + "<button class='btn btn-primary' type='submit' onClick='return getResults(" + nbq + ");'> Envoyer mes réponses </button>"
+            reponse + "<button class='btn btn-primary' type='submit' onClick='return getResults(" + nbq + ", \""+ id+"\");'> Envoyer mes réponses </button>"
 
     }
     xmlhttp.open("GET", "../data/quizz.json");
     xmlhttp.send();
 }
 
-function getResults(nbquestions) {
 
+function getResults(nbquestions, id) {
+
+    let score = 0;
 
     // LOOP FOR GOING THROUGH ALL QUESTIONS
     for (let i = 0; i < nbquestions; i++) {
         let radiosName = document.getElementsByName('question' + i);
         radiosName.disabled = true;
+
         //LOOP FOR CHECKING ANSWERS INSIDE EACH RADIO
         for (var j = 0; j < radiosName.length; j++) {
 
             let label = document.getElementById("labelq" + i + "r" + j)
-            
             var radiosValue = radiosName[j];
-            if (radiosValue.value ==  quizz_array[0].questions[i].reponse_exacte && radiosValue.checked) {
-               
+            if (radiosValue.value == quizz_array[0].questions[i].reponse_exacte && radiosValue.checked) {
+
                 radiosValue.style.backgroundColor = "green";
                 label.style.color = "green";
+                score = score + 1;
             }
 
             if (radiosValue.value != quizz_array[0].questions[i].reponse_exacte && radiosValue.checked) {
@@ -63,11 +67,16 @@ function getResults(nbquestions) {
 
             }
 
-            if (radiosValue.value ==  quizz_array[0].questions[i].reponse_exacte){
+            if (radiosValue.value == quizz_array[0].questions[i].reponse_exacte) {
                 label.style.backgroundColor = "#b1d9b1";
                 label.style.padding = "1.5px";
 
             }
         }
     }
+
+    document.location.href = "post-quizz.php?id=" + id + "&score=" + score + "/" + nbquestions;
+    //document.getElementById('score').innerHTML = "<p>Votre score : " + score + "/" + nbquestions + " </p>"
+
 }
+
