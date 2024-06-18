e09754ffce883b4fd138b5917f13bfbbfd1529e1
safiylm <121391404+safiylm@users.noreply.github.com>
2024-06-18 11:37:00 +0200

    add id & resultat of all ?

--- quizz/post-quizz.php
+++ quizz/post-quizz.php
@@ -5,12 +5,13 @@ $json_object = file_get_contents($path);
 $tab = json_decode($json_object, true);
 
 $newScore = array();
+$newScore['id'] = end($tab["quizscore"])['id'] + 1;
 $newScore['userId'] = 1;
 $newScore["quizId"] = $_GET['id'];
 $newScore["score"] = $_GET["score"];
+$newScore["resultat"] = $_GET["resultat"];
 array_push($tab["quizscore"], $newScore);
-
-print_r($newScore);
+// print_r($newScore);
 
 $jsonString = json_encode($tab, JSON_PRETTY_PRINT);
 // // Write in the file
@@ -18,5 +19,4 @@ $fp = fopen($path, 'w');
 fwrite($fp, $jsonString);
 fclose($fp);
 
-//echo "<script>document.location.href='index.php?id=".  $_GET['id']."&resultat=". $_GET['score'] ."';</script>"
-?>
+ echo "<script>document.location.href='resultat.php?idres=" . $newScore['id'] . "';</script>";
