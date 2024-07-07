5634a8ff8ef680f65a732c0c6e2fb0f1aa4a808c
safiylm <121391404+safiylm@users.noreply.github.com>
2024-07-07 13:41:53 +0200

    update nav

--- shared/nav.php
+++ shared/nav.php
@@ -8,7 +8,11 @@
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="../index.php">Home</a>
+          <a class="nav-link" href="../index.php">Articles</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="../quizz.php">Quizz</a>
         </li>
 
         <?php if (!empty($_SESSION['user']['email'])) { ?>
