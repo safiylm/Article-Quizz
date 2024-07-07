19c43e7884963829b9a1ce7466a7814581504546
safiylm <121391404+safiylm@users.noreply.github.com>
2024-07-07 13:42:22 +0200

    separate Quizz & articles in 2 files

--- index.php
+++ index.php
@@ -27,15 +27,20 @@
 
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#"> Article & Quizz </a>
+            <a class="navbar-brand" href="index.php"> Article & Quizz </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <a class="nav-link" href="index.php">Articles</a>
                     </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="quizz.php">Quizz</a>
+                    </li>
+
 
                     <?php if (!empty($_SESSION['user']['email'])) { ?>
                         <li class="nav-item">
@@ -56,61 +61,24 @@
         </div>
     </nav>
 
-    <div class="container-grid">
 
-        <div id="item-menu">
-            <button id="btn-open-listesarticles">Articles</button>
-            <button id="btn-open-listesquizz">Quizz</button>
+    <div id="div-container-articles">
+        <h1>Articles</h1>
+
+        <div id="liste-of-articles" class="d-flex flex-wrap align-items-center justify-content-center">
+            <?php
+            foreach ($json_articles["listeOfArticles"] as $elment) {
+                article_card($elment);
+            }
+            ?>
         </div>
-
-
-        <div id="item-main">
-
-
-            <div id="div-container-articles">
-                <h1>Nos articles</h1>
-
-                <div id="liste-of-articles" class="d-flex flex-wrap align-items-center justify-content-center">
-                    <?php
-                    foreach ($json_articles["listeOfArticles"] as $elment) {
-                        article_card($elment);
-                    }
-                    ?>
-                </div>
-            </div>
-            <div id="div-container-quizz">
-
-                <h1>Quizz</h1>
-
-                <div id="liste-of-articles" class="d-flex flex-wrap align-items-center justify-content-center">
-                    <?php
-                    foreach ($json_quizz["listeOfQuizz"] as $elment) {
-                        quizz_card($elment);
-                    }
-                    ?>
-                </div>
-            </div>
-        </div>
-
     </div>
+
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script>
-      $("#div-container-quizz").hide();
-
-        $("#btn-open-listesarticles").click(()=>{
-            $("#div-container-quizz").hide();
-            $("#div-container-articles").show();
-
-        })
-        $("#btn-open-listesquizz").click(()=>{
-            $("#div-container-quizz").show();
-            $("#div-container-articles").hide();
-
-        })
-    </script>
+   
 </body>
 
 </html>
